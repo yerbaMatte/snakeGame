c0011a46690f183f78570fc32944ca8cd37534b4
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2022-09-24 17:17:53 +0200

    first snake full-moving version, food basics

--- script.js
+++ script.js
@@ -1,53 +1,79 @@
-'use sctrict';
+'use strict';
 
-const cube = document.querySelector('.cube');
-
+const snake = document.querySelector('.snake');
+let xORy = '';
+let addSubs;
 //Initial position
 let cubePosition = {
   x: 0,
   y: 0,
 };
-
 let transform = '';
+let zero = 0;
 
 //addEvent Arrows
 addEventListener('keydown', function (e) {
   if (e.key === 'ArrowUp') {
-    cubePosition.y -= 50;
-    ycheck();
+    xORy = 'y';
+    addSubs = -50;
+    cubePosition[xORy] += addSubs;
+    checker();
   }
   if (e.key === 'ArrowDown') {
-    cubePosition.y += 50;
-    ycheck();
+    xORy = 'y';
+    addSubs = 50;
+    cubePosition[xORy] += addSubs;
+    checker();
   }
   if (e.key === 'ArrowLeft') {
-    cubePosition.x -= 50;
-    xcheck();
+    xORy = 'x';
+    addSubs = -50;
+    cubePosition[xORy] += addSubs;
+    checker();
   }
   if (e.key === 'ArrowRight') {
-    cubePosition.x += 50;
-    xcheck();
+    xORy = 'x';
+    addSubs = 50;
+    cubePosition[xORy] += addSubs;
+    checker();
   }
-  transform = `translate(${cubePosition.x},${cubePosition.y})`;
-  cube.setAttribute('transform', transform);
-  console.log(cubePosition, transform);
+  updatePosition();
+  console.log(cubePosition, transform, xORy);
 });
 
-//check if x
-function xcheck() {
-  if (cubePosition.x > 450) {
-    cubePosition.x = 0;
+function checker() {
+  //check if x
+  function xcheck() {
+    if (cubePosition.x > 450) {
+      cubePosition.x = 0;
+    }
+    if (cubePosition.x < 0) {
+      cubePosition.x = 450;
+    }
   }
-  if (cubePosition.x < 0) {
-    cubePosition.x = 450;
+  //check if y
+  function ycheck() {
+    if (cubePosition.y > 450) {
+      cubePosition.y = 0;
+    }
+    if (cubePosition.y < 0) {
+      cubePosition.y = 450;
+    }
   }
+  xcheck();
+  ycheck();
 }
 
-function ycheck() {
-  if (cubePosition.y > 450) {
-    cubePosition.y = 0;
-  }
-  if (cubePosition.y < 0) {
-    cubePosition.y = 450;
-  }
+//update-position
+function updatePosition() {
+  transform = `translate(${cubePosition.x},${cubePosition.y})`;
+  snake.setAttribute('transform', transform);
+}
+
+const moving = setInterval(move, 1000);
+
+function move() {
+  cubePosition[xORy] += addSubs;
+  checker();
+  updatePosition();
 }
